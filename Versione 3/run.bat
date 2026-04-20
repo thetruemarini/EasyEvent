0cc38c9ce4fc521f0171db38a9d5bc4fe40d767a
Raffaele Principe <raffaele.principe03@gmail.com>
2026-04-20 23:32:40 +0200

    Pulizia branch: rimosse vecchie versioni, mantenuta solo versione 5

--- Versione 3/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM EasyEvent V3 - Script di compilazione ed esecuzione (Windows)
-mkdir out 2>nul
-javac -d out @sources.txt && java -cp out it.easyevent.MainV3
--- Versione 3/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# EasyEvent V3 - Script di compilazione ed esecuzione (Linux/macOS)
-mkdir -p out
-javac -d out @sources.txt && java -cp out it.easyevent.MainV3
